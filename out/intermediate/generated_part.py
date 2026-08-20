76c00c2ae3bc6f35280c9b6049844f89ffa06cdd
Brahim Khalil Kaak <brahimkhalilkaak@hotmail.com>
2026-08-20 18:00:35 +0100

    Ignore python generated files (out/, pycache)

--- out/intermediate/generated_part.py
+++
@@ -1,8 +0,0 @@
-from build123d import Box, BuildPart, export_stl
-from ocp_vscode import show
-
-with BuildPart() as part:
-    Box(10.0, 10.0, 10.0)
-
-show(part.part, reset_camera=False, axes=True, grid=True)
-export_stl(part.part, "generated_part.stl")
